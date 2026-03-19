1c62fd6823aa555bccc7f57c01b6340bdd1ff6cf
jacobj7 <68085132+jacobj7@users.noreply.github.com>
2026-03-18 23:57:50 -0400

    fix: add query helper to lib/db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,17 @@
-import { Pool } from "pg";
+import { Pool, QueryResult } from "pg";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
+
+export async function query(text: string, params: any[]): Promise<QueryResult> {
+  const client = await pool.connect();
+  try {
+    return await client.query(text, params);
+  } finally {
+    client.release();
+  }
+}
 
 export { pool };
 export const db = pool;
